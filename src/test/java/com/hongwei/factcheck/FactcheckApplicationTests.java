9e9b8beaf8e92ffe0cd3b2ab557e153570411855
Hong Wei Tai <taiih@192.168.1.33>
2025-10-14 01:51:42 +0800

    Initial commit

---
+++ src/test/java/com/hongwei/factcheck/FactcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongwei.factcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
